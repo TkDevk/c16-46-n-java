f5ea550717ce5b6258938426828adf0e69756e81
FacundoLB <facuu142@gmail.com>
2024-02-11 13:03:06 -0300

    proyecto Spring

---
+++ src/main/java/com/c1646njava/tuvivienda/TuviviendaApplication.java
@@ -0,0 +1,13 @@
+package com.c1646njava.tuvivienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuviviendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuviviendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c1646njava/tuvivienda/TuviviendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c1646njava.tuvivienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuviviendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
