778b01356ec7bf7f2bcad9563b96933aeeb29773
Martinm1500 <mm389358@gmail.com>
2024-02-14 18:34:27 -0300

    Repositories for User, Administrator, and Post were created. The model for Administrator was created.

---
+++ Back-End/src/main/java/com/c1646njava/tuvivienda/repositories/AdministratorRepository.java
@@ -0,0 +1,7 @@
+package com.c1646njava.tuvivienda.repositories;
+
+import com.c1646njava.tuvivienda.models.Administrator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdministratorRepository extends JpaRepository<Administrator, Long> {
+}
