40596aa5382b2ff3784351275fa01ebffc854c3f
FacundoLB <facuu142@gmail.com>
2024-02-14 22:40:59 -0300

    reorganizando carpetas

--- Back-End/src/main/java/com/c1646njava/tuvivienda/services/abstraction/UserService.java
+++ Back-End/src/main/java/com/c1646njava/tuvivienda/services/abstraction/UserService.java
@@ -1,7 +1,15 @@
 package com.c1646njava.tuvivienda.services.abstraction;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
-    
-    
+
+    Long registerUser(String name, String Password,String email);
+    Boolean loginUser(String email, String Password);
+    Boolean logoutUser();
+    Boolean checkSession();
+
+
 
 }
