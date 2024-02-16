8549c0cb11c1826efd57f72d07768dd1c4faf9e8
k-alex-leon <kelemon824@gmail.com>
2024-02-15 20:45:52 -0500

    Navbar Updated

--- Client/src/feature/components/NavBar.jsx
+++ Client/src/feature/components/NavBar.jsx
@@ -1,62 +1,75 @@
-
 import { FcHome } from "react-icons/fc";
 import { PiCalendarDuotone } from "react-icons/pi";
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { BiMessageRoundedDots } from "react-icons/bi";
 import { BsPersonCircle } from "react-icons/bs";
 import { CgMenuRound } from "react-icons/cg";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { HomeContext } from "../../context/HomeContext";
 
-const NavBar = () => {
+const sizeFont = "1.5rem"
 
+const navItems = [
+  {
+    icon: <PiCalendarDuotone style={{ fontSize: sizeFont }} />,
+    name: "calendar",
+  },
+  {
+    icon: <MdOutlineNotificationsActive style={{ fontSize: sizeFont }} />,
+    name: "notifications",
+  },
+  {
+    icon: <BiMessageRoundedDots style={{ fontSize: sizeFont }} />,
+    name: "messages",
+  },
+  {
+    icon: <CgMenuRound style={{ fontSize: sizeFont }} />,
+    name: "settings",
+  },
+  {
+    icon: <BsPersonCircle style={{ fontSize: sizeFont }} />,
+    name: "login",
+  },
+];
+
+const NavBar = () => {
   const { handlerOpenSidebar } = useContext(HomeContext);
 
   return (
     <>
-      <nav className={`absolute top-0 w-full h-12vh flex p-3 pr-3 pl-5 justify-between z-60`}>
-        <section className="flex">
-          <h1 className="flex text-xl">
+      <nav
+        className={`absolute top-0 w-full h-12vh flex p-3 pr-3 pl-5 z-60`}
+      >
+        <section className="flex items-center">
+          <h1 className="flex md:text-xl">
             <a href="./">
-                <picture 
-                >
-                <FcHome 
-                style={{fontSize:'2.5rem'}}/>                
-                </picture>
-                <span className={`collapse md:absolute top-5 pl-12`}
-                >TUVIVIENDA.com</span>
+              <picture>
+                <FcHome style={{ fontSize: "1.5rem" }} />
+              </picture>
+              <span className={`invisible md:visible top-5 pl-12`}>
+                TUVIVIENDA.com
+              </span>
             </a>
           </h1>
         </section>
-        <section className="flex p-2 border border-black rounded-full
-        gap-4
-        ">
-          <Link to="/calendar" onClick={handlerOpenSidebar}>
-            <picture>
-              <PiCalendarDuotone style={{fontSize:'2rem', cursor:'pointer'}}/>
-            </picture>
-          </Link>
-          <Link to="/notifications" name={'notifications'} onClick={handlerOpenSidebar}>
-            <picture>
-              <MdOutlineNotificationsActive style={{fontSize:'2rem', cursor:'pointer'}}/>
-            </picture>
-          </Link>
-          <Link to="/messages" name={'messages'} onClick={handlerOpenSidebar}>
-            <picture>
-              <BiMessageRoundedDots style={{fontSize:'2rem', cursor:'pointer'}}/>
-            </picture>
-          </Link>
-          <Link to="/settings" name={'settings'} onClick={handlerOpenSidebar}>
-            <picture>
-              <CgMenuRound style={{fontSize:'2rem', cursor:'pointer'}}/>
-            </picture>
-          </Link>
-          <Link to="/login" name={'login'} onClick={handlerOpenSidebar}>
-            <picture>
-              <BsPersonCircle style={{fontSize:'2rem', cursor:'pointer'}}/>
-            </picture>
-          </Link>
+        <section
+          className="flex items-center rounded-full gap-4 justify-center"
+        >
+          {navItems.map((item) => {
+            return (
+              <Link
+                to={`/${item.name}`}
+                className="cursor-pointer"
+                name={item.name}
+                onClick={handlerOpenSidebar}
+              >
+                <picture>
+                  {item.icon}
+                </picture>
+              </Link>
+            );
+          })}
         </section>
       </nav>
     </>
